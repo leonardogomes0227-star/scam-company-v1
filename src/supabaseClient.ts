80b2ce9cd642aae3b78a8f990f7b8171d407c9fb
leonardogomes0227-star <leonardogomes0227@gmail.com>
2026-07-27 23:20:33 -0300

    Update supabaseClient.ts

--- src/supabaseClient.ts
+++ src/supabaseClient.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = 'https://ezbhiyzcdkszeazzhsyn.supabase.co'
-const supabaseKey = 'SUA_PUBLISHABLE_KEY_AQUI' // Cole a chave que está no print
+const supabaseKey = 'sb_publishable_3LbKgY81gZCuoqclvjg_5zqnn...' // Cole a chave completa aqui
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
