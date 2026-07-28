089527af27d0a0b8693052a6bf1c19914b9a8558
leonardogomes0227-star <leonardogomes0227@gmail.com>
2026-07-27 23:23:01 -0300

    Update supabaseClient.ts

--- src/supabaseClient.ts
+++ src/supabaseClient.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = 'https://ezbhiyzcdkszeazzhsyn.supabase.co'
-const supabaseKey = 'sb_publishable_3LbKgY81gZCuoqclvjg_5zqnn...' // Cole a chave completa aqui
+const supabaseKey = 'cole_a_sua_chave_real_aqui_entre_as_aspas' 
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
